4abb10c51d6563d46dbfe30486c2d8ba55d6fb56
Eduard <melkon1999@gmail.com>
2022-02-07 17:51:06 +0400

    updated

--- queue.js
+++ queue.js
@@ -1,17 +1,17 @@
 let arr = [];
-function queue(arr, action, elem){
-if(action == 'set'){
+function queue(arr, action, elem) {
+  if (action == "set") {
     arr.push(elem);
     console.log(arr);
-}
-if(action == 'get'){
+  }
+  if (action == "get") {
     console.log(arr[0]);
     arr.shift();
+  }
 }
-}
-queue(arr, 'set', 5)
-queue(arr, 'set', 10)
-queue(arr, 'set', 15)
-queue(arr, 'get')
-queue(arr, 'get')
-queue(arr, 'get')
+queue(arr, "set", 5);
+queue(arr, "set", 10);
+queue(arr, "set", 15);
+queue(arr, "get");
+queue(arr, "get");
+queue(arr, "get");
